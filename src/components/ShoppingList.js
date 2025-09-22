02ca88980e6d56668335f7495808fcf72fcb8f31
Dean Baberowski <deanbaberows@gmail.com>
2025-09-22 11:13:40 +1000

    finished codealong

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,14 +1,44 @@
-import React, { useState } from "react";
-import ItemForm from "./ItemForm";
+import { useEffect, useState } from "react";
 import Filter from "./Filter";
 import Item from "./Item";
+import ItemForm from "./ItemForm";
 
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  function handleUpdateItem(updatedItem) {
+   const updatedItems = items.map((item) => {
+    if (item.id === updatedItem.id) {
+      return updatedItem;
+    } else {
+      return item;
+    }
+  });
+  setItems(updatedItems);
+}
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
+  }
+
+  function handleAddItem(newItem) {
+    console.log("In ShoppingList:", newItem);
+  }
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+    console.log("In Shopping List:", deletedItem);
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -19,14 +49,17 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+       <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} 
+          onUpdateItem={handleUpdateItem}
+          onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
